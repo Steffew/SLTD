a486ad4a50c9ab9739bc2d2ee1be66c52a34bbc0
Steffew <xgenyofficial@gmail.com>
2019-12-18 10:03:31 +0100

    Update comment

--- Assets/Scripts/WayPoint.cs
+++ Assets/Scripts/WayPoint.cs
@@ -9,10 +9,11 @@ public class WayPoint : MonoBehaviour
     private void Awake()
     {
         points = new Transform[transform.childCount];
-            for (int i = 0; i < points.Length; i++)
+        for (int i = 0; i < points.Length; i++)
         {
             points[i] = transform.GetChild(i);
         }
     }
 
 }
+//hoi
